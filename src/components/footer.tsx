4e287066f0e167f980d66901731aec2d3f6bde18
Alicia G <alicia.guardenoalbertos@gmail.com>
2019-11-25 21:37:47 +0100

    minor fix footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+const React = require("react");
+const ReactDOM =  require("react-dom");
 const classes = require('../sass/style.scss');
 
 ReactDOM.render(
